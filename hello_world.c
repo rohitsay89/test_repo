c2625fa502b81d790c9db404bdfeb06b03da273d
rohitsay89 <rohitsay89@gmail.com>
2017-06-19 17:04:00 -0400

    multimplication in branch 1

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+// branch 1 code
+int a = 10;
+int b = 20;
+
 int main(int argc, char **argv)
 {
-	printf("Hellow World !!");
+	printf("multiplication of 2 numbers is : ", a*b);
 	return 0;
 }
