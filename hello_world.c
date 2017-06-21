c0301b37533fa594c105a436e76f7b70efb43f89
rohitsay89 <rohitsay89@gmail.com>
2017-06-21 11:23:40 -0400

    adding functions for addition and multiplicataion to branch 1

--- hello_world.c
+++ hello_world.c
@@ -4,8 +4,19 @@
 int a = 10;
 int b = 20;
 
+int add( int a, int b )
+{
+	return ( a + b );
+}
+
+int mul( int a , int b)
+{
+	return ( a * b );
+}
+
 int main(int argc, char **argv)
 {
-	printf("multiplication of 2 numbers is : ", a*b);
+	printf("multiplication of 2 numbers is : %d \n", mul( a, b ));
+	printf("addition is :%d \n", add( a , b ));
 	return 0;
 }
